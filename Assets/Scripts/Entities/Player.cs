c63cfb0484913c25daea5d2e15428ff78980149b
Richard Olsen <olsen7781@gmail.com>
2022-04-02 14:17:32 -0600

    Changed gun, commented out gun debug control

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -21,15 +21,15 @@ public class Player : Entity
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
-        {
-            playersArm.dropWeapon();
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            GameObject obj = GameObject.Instantiate(spawnThingy);
-            giveWeapon(obj.GetComponent<Gun>());
-        }
+        //if (Input.GetKey(KeyCode.Q))
+        //{
+        //    playersArm.dropWeapon();
+        //}
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    GameObject obj = GameObject.Instantiate(spawnThingy);
+        //    giveWeapon(obj.GetComponent<Gun>());
+        //}
     }
 
     public void giveWeapon(Gun gun)
