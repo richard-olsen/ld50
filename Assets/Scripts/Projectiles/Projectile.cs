10fdf344ce50e490797d69f894a32791797b939b
Levi Shafter <shafterinc1@gmail.com>
2022-04-02 00:48:44 -0600

    moved damage to projectile/melee

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -26,11 +26,13 @@ public class Projectile : MonoBehaviour
     {
         body.MovePosition(body.position + direction * speed * Time.deltaTime);
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision2D collision)
     {
-        if (collision.collider.GetComponent<Entity>())
+        Collider2D collObj = collision.collider;
+        if (collObj.GetComponent<Entity>())
         {
             Destroy(gameObject);
+                collObj.gameObject.GetComponent<Entity>().hp -= 1;
         }
     }
 }
