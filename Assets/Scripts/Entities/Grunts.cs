20e685f4b8dfaf64ef156474008686a1fff8ffb5
Levi Shafter <shafterinc1@gmail.com>
2022-04-02 17:08:56 -0600

    added melee to grunt

--- Assets/Scripts/Entities/Grunts.cs
+++ Assets/Scripts/Entities/Grunts.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Grunts : Entity
 {
+    private Animator anim;
+    public float attackTime;
+    public float startTimeAttack;
+
+    public Transform attackLocation;
+    public float attackRange;
+    public LayerMask player;
+
     // Start is called before the first frame update
     void Start()
     {
         giveHealth(3);
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //melee attack
+        if (attackTime <= 0)
+        {
+            if (!(gameObject.GetComponent<Player>() is null) && Vector2.Distance(this.transform.position,gameObject.GetComponent<Player>().transform.position) < 1) //FIXME: distance comparison must be updated once we know length of weapon/arms
+            {
+                anim.SetBool("Is_attacking", true);
+                Collider2D[] damage = Physics2D.OverlapCircleAll(attackLocation.position, attackRange, player);
+
+                for (int i = 0; i < damage.Length; i++)
+                {
+                    Player player = damage[i].gameObject.GetComponent<Player>();
+                    if (!(player is null))
+                        player.damage(4);
+                }
+            }
+            attackTime = startTimeAttack;
+        }
+        else
+        {
+            attackTime -= Time.deltaTime;
+            anim.SetBool("Is_attacking", false);
+        }
     }
 }
